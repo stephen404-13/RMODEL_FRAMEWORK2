2bb0a03fda93153cd74f640cd091635a23addf8e
Stephane Tadjou <fotsingtadjou@gmail.com>
2021-12-31 21:27:25 +0100

    add biblio pandas

--- RMFrameClasse/resources.py
+++ RMFrameClasse/resources.py
@@ -4,9 +4,10 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import tree
 from sklearn.pipeline import make_pipeline
-
-
+import pandas as pd
 
 
 hyper_params_svm = {
@@ -21,8 +22,36 @@ hyper_params_logistic = {
     'pipeline__polynomialfeatures__degree': [2, 3, 4],
     'pipeline__selectkbest__k': range(4, 100)
 }
+hyper_params_knn = {}
+
+hyper_params_radomForest = {}
+
+hyper_params_MLP = {}
+
+hyper_params_AdaBoost = {}
+
+hyper_params_lda = {
+    'lda__solver' : ['svd', 'lsqr', 'eigen'],
+    'lda__shrinkage' : ['auto','float'] ,
+    'pipeline__polynomialfeatures__degree': [2, 3, 4],
+    'pipeline__selectkbest__k': range(4, 100)
+}
+
+hyper_params_tree = {
+    'pipeline__polynomialfeatures__degree': [2, 3, 4],
+    'pipeline__selectkbest__k': range(4, 100)
+}
+
+
 
 
 
 ##===================LIEN DU FICHIER DE TEST====================#
-source = r'train_u6lujuX_CVtuZ9i.csv'
+source2 = r'train_u6lujuX_CVtuZ9i.csv'
+source = r'chunk.csv'
+
+##==================New dataset customer ========================
+new_dataset = r'new_customers.csv'
+
+df_new = pd.read_csv(new_dataset)
+df_new_scustomer = df_new.drop(['Churn'], axis=1)
--- RMFrameClasse/test_scoring.py
+++ RMFrameClasse/test_scoring.py
@@ -1,27 +1,35 @@
-from biblioFramework import DataImport
-
-from biblioFramework import PreprocessingData
-
-from  biblioFramework import Scoring
-
+from RModelFramework import DataImport
+from RModelFramework import PreprocessingData
+from  RModelFramework import Scoring
 from resources import *
 
 
 if __name__ == "__main__":
+    #pd.set_option('display.max_columns', None)
 
     data = DataImport(source)
-
     data.chargement()
+    data.display_data(20)
 
-    dataset = data.delete_data_entry("Loan_ID",axis=1)   
 
-    preprocessor = PreprocessingData(dataset)
+    #target = 'Loan_Status'
+    # dataset = data.delete_data_entry("Loan_ID",axis=1)
 
-    preprocessor.encodage_label()
 
-    dataset = preprocessor.dataFrame
+    target = "Churn"
+    dataset = data.delete_data_entry("customerID", axis=1)
 
-    preprocessor = preprocessor.pipelinePreprocessing()
+    print(dataset.columns)
+    preprocessor1 = PreprocessingData(dataset,target)
+
+    preprocessor1.encodage_label()
+
+    dataset = preprocessor1.dataFrame
+
+    print(dataset.head(20))
+
+    preprocessor = preprocessor1.pipelinePreprocessing()
+
 
     ###################### INITIALISATION DES Algorithmes NECESSAIRES POUR LE SCORING #################
 
@@ -31,35 +39,45 @@ if __name__ == "__main__":
     KNN = make_pipeline(preprocessor, KNeighborsClassifier())
     Logistic = make_pipeline(preprocessor, LogisticRegression(random_state=0))
     MLP = make_pipeline(preprocessor, MLPClassifier())
+    LDA = make_pipeline(preprocessor,LinearDiscriminantAnalysis())
+    Binary_tree = make_pipeline(preprocessor,tree.DecisionTreeClassifier())
+
+
 
     #####Dictionnaire des Algorithmes avec leurs hyperparamètres
+
     list_of_model = {
-        "RandomForest": [RandomForest, {}],
-        "AdaBoost": [AdaBoost, {}],
-        "SVM": [SVM, hyper_params_svm],
-        "KNN": [KNN, {}],
+        "RandomForest": [RandomForest, hyper_params_radomForest],
+        #"AdaBoost": [AdaBoost, hyper_params_AdaBoost],
+        #"SVM": [SVM, hyper_params_svm],
+        #"KNN": [KNN, hyper_params_knn],
         "Logistic": [Logistic, hyper_params_logistic],
-        "MLP": [MLP, {}]
+        "MLP": [MLP, hyper_params_MLP],
+        "LDA":[LDA,hyper_params_lda],
+        "Binary_tree":[Binary_tree,hyper_params_tree]
     }
 
+    scoring = Scoring(list_of_model,dataset,target)
 
-    scoring = Scoring(list_of_model,dataset)
+    #pair_plot = scoring.matrix_corelation()
 
-    pair_plot = scoring.matrix_corelation()
+    #print(pair_plot)
 
+    performences_models,best_model = scoring.executer()
 
-    val = scoring.executer()
-
-    print("le meilleur model est : ",val)
+    print("la performence de tous les models  : ",performences_models)
+    print("Le meilleur model est models:",best_model)
 
     scoring.optimisationHyperParam()
 
-
-    score_dataFrame = scoring.scoring()
-
-
     scoring.save_model()
 
+    scoring.importance_features()
 
+    score_dataFrame = scoring.scoring()
+    #====================SCORING SUR DE NOUVEAU DONNEES ======================"=====
+
+
+    score_dataFrame = scoring.scoring_new_data(df_new_scustomer)
 
     print(score_dataFrame)
